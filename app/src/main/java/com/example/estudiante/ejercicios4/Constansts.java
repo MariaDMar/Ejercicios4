3b39aa6412c4ac622af40451ff807cd5e4863ee3
Isabella Jordan <isabellajordanbe@gmail.com>
2018-08-23 13:05:44 -0500

    cambios para finish

--- app/src/main/java/com/example/estudiante/ejercicios4/Constansts.java
+++ app/src/main/java/com/example/estudiante/ejercicios4/Constansts.java
@@ -3,5 +3,6 @@ package com.example.estudiante.ejercicios4;
 public class Constansts {
 
    public static final int REQ_NEW_USER = 5000;
+   public static final int RESP_NEW_USER = 5001;
 
 }
--- app/src/main/java/com/example/estudiante/ejercicios4/UserFormActivity.java
+++ app/src/main/java/com/example/estudiante/ejercicios4/UserFormActivity.java
@@ -3,6 +3,8 @@ package com.example.estudiante.ejercicios4;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class UserFormActivity extends AppCompatActivity {
@@ -10,6 +12,8 @@ public class UserFormActivity extends AppCompatActivity {
     private EditText edtUserFormName;
     private EditText edtUserFormEmail;
     private EditText edtUserFormUserName;
+
+    private Button btnUserFormFinish;
 
 
 
@@ -22,5 +26,24 @@ public class UserFormActivity extends AppCompatActivity {
         edtUserFormName = findViewById(R.id.edt_userform_name);
         edtUserFormEmail = findViewById(R.id.edt_userform_email);
         edtUserFormUserName = findViewById(R.id.edt_userform_username);
+
+        btnUserFormFinish = findViewById(R.id.btn_user_form_finish);
+
+        btnUserFormFinish.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent respuesta = new Intent();
+
+                String name = edtUserFormName.getText().toString();
+                String email = edtUserFormEmail.getText().toString();
+                String username= edtUserFormUserName.getText().toString();
+
+                respuesta.putExtra("name", name);
+                respuesta.putExtra("email", email);
+                respuesta.putExtra("username", username);
+
+                setResult(Constansts.RESP_NEW_USER, respuesta);
+                finish();
+            }
+        });
     }
 }
